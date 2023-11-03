d1c636f1a8fe85e4cfe63eef3460904cd75c0e7f
Yassmin Mouaddeb <98842040+ElMeddebYassmin@users.noreply.github.com>
2023-11-03 01:35:08 +0100

    Nexus Config

--- src/main/java/com/example/testdemo/TestDemoApplication.java
+++ src/main/java/com/example/testdemo/TestDemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.testdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SpringBootConfiguration
 public class TestDemoApplication {
 
     public static void main(String[] args) {
